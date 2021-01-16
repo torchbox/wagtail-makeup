5b1f673833e9ece697e8855580188b5778683d91
k <kbhow@protonmail.com>
2021-01-16 14:11:00 +0000

    Starting tests

--- wagtailmakeup/tests/tests.py
+++ wagtailmakeup/tests/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Add your tests here
+class MakeupTest(TestCase):
+    def test_math(self):
+        self.assertEqual(1,1)
+
+    def test_math_wrong(self):
+        self.assertEqual(2,1)
+
+
+    # Test settings are defined, if not show a warning
+
+    # Test image file is replaced
+
