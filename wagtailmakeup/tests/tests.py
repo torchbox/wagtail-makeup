8d4e11920278cb076d4f5c023603299f9a6eb478
k <kbhow@protonmail.com>
2021-01-17 13:04:47 +0000

    tests

--- wagtailmakeup/tests/tests.py
+++ wagtailmakeup/tests/tests.py
@@ -4,10 +4,6 @@ class MakeupTest(TestCase):
     def test_math(self):
         self.assertEqual(1,1)
 
-    def test_math_wrong(self):
-        self.assertEqual(2,1)
-
-
     # Test settings are defined, if not show a warning
 
     # Test image file is replaced
