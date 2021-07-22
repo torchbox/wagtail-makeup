50627f8aa44f53399cd187b52aa5b3d803e43ca4
Kevin Howbrook <kbhowbrook@gmail.com>
2021-07-22 22:02:55 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 
 setup(name='wagtailmakeup',
-      version='0.0.1',
+      version='0.0.2',
       description='Wagtail Makeup',
       long_description=long_description,
       long_description_content_type='text/markdown',
