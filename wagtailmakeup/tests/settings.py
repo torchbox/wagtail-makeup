2748660499d0625d4af4bf66814e551120486c77
katdom13 <katdom13@gmail.com>
2024-05-27 19:18:46 +0800

    Wagtail 3.0 upgrade consideration: All other modules under wagtail.core can now be found under wagtail

--- wagtailmakeup/tests/settings.py
+++ wagtailmakeup/tests/settings.py
@@ -47,7 +47,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "wagtail.core.middleware.SiteMiddleware",
 ]
 
 ROOT_URLCONF = "wagtailmakeup.tests.urls"
